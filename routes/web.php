51f7c723dd3770930752953e1b1df50e31bc5ce8
abin1769 <abin.shafwan@gmail.com>
2026-05-29 16:11:01 +0700

    Adding first auth system

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/pemilik', function () {
-    return view('pemilik.dashboard');
+Route::get('/', function () {
+    return view('pemilik.guest');
 });
 
-Route::get('/guest', function () {
-    return view('pemilik.guest');
-})->name('pemilik.guest');
+Route::get('/dashboard', function () {
+    return view('pemilik.dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
